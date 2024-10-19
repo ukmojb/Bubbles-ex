2519e259a213dcb741e4aa90c622f06119b27731
CaliforniaDemise <vwwwwv@proton.me>
2024-10-19 20:50:59 +0300

    Make BaubleItem wearable with right click.

--- src/main/java/baubles/api/cap/BaubleItem.java
+++ src/main/java/baubles/api/cap/BaubleItem.java
@@ -1,9 +1,18 @@
 package baubles.api.cap;
 
 import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
 import baubles.api.IBauble;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 /**
  * Pretty basic implementation of a bauble item
@@ -19,5 +28,27 @@ public class BaubleItem extends Item implements IBauble {
     @SuppressWarnings("deprecation")
     public BaubleType getBaubleType(ItemStack itemstack) {
         return baubleType;
+    }
+
+    @Nonnull
+    @Override
+    @ParametersAreNonnullByDefault
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        boolean check = false;
+        IBaublesItemHandler handler = BaublesApi.getBaublesHandler(playerIn);
+        for (int i = 0; i < handler.getSlots(); i++) {
+            if (handler.isItemValidForSlot(i, stack, playerIn)) {
+                check = true;
+                if (!worldIn.isRemote) {
+                    handler.setStackInSlot(i, stack.copy());
+                    stack.shrink(1);
+                }
+                break;
+            }
+        }
+        this.onEquipped(stack, playerIn);
+        if (check) return new ActionResult<>(EnumActionResult.SUCCESS, stack);
+        else return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
